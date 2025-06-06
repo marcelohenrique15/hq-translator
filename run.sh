70ccac5231dfea0c94cb02a2d490f61e5d2e1738
Marcelo Henrique <marcelohenriquepessoal15@gmail.com>
2025-06-05 22:56:42 -0300

    CHORE: Run update

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-rm -rf build && mkdir build && cd build
+if [ -d "build" ]; then
+  echo "\nBuild já foi criada.\n"
+
+else
+  echo "\nCriando pasta Build...\n"
+  mkdir build
+fi
+
+echo "\nCompilando...\n"
+
+cd build
 cmake ..
 make
 ./main
